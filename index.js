70dc8ae6c13023234bc5d17d4fd5659abab4e4c8
mzain7 <mzain.akhtar3@gmail.com>
2023-12-11 22:00:28 +0500

    package changed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { translate } from "@vitalets/google-translate-api";
-import { HttpProxyAgent } from "http-proxy-agent";
+// import { translate } from "@vitalets/google-translate-api";
+// import { HttpProxyAgent } from "http-proxy-agent";
 
-const agent = new HttpProxyAgent("http://162.223.94.164:80");
-import express from "express";
+// const agent = new HttpProxyAgent("http://162.223.94.164:80");
+// import express from "express";
+const translate = require('@iamtraction/google-translate');
+const express = require('express');
+
 const app = express();
 const router = express.Router();
 
@@ -10,13 +13,13 @@ app.use(express.json());
 
 router.post("/translate", async (req, res) => {
   const { text, lang } = req.body;
-  // console.log(text, to, from);
+  console.log(text, lang);
   const result = await translate(text, {
     to: lang,
     from: "en",
-    fetchOptions: { agent },
+    // fetchOptions: { agent },
   });
-  res.send(result.text);
+  res.json(result.text).status(200);
 });
 
 app.use("/api", router);
