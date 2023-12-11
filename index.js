2ef0dabb74c6a194d7b8c6bf22ba2ae91f344f22
mzain7 <mzain.akhtar3@gmail.com>
2023-12-11 22:19:20 +0500

    updated

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// import { translate } from "@vitalets/google-translate-api";
-// import { HttpProxyAgent } from "http-proxy-agent";
 
-// const agent = new HttpProxyAgent("http://162.223.94.164:80");
-// import express from "express";
 const translate = require('@iamtraction/google-translate');
 const express = require('express');
 
@@ -17,7 +13,6 @@ router.post("/translate", async (req, res) => {
   const result = await translate(text, {
     to: lang,
     from: "en",
-    // fetchOptions: { agent },
   });
   res.json(result.text).status(200);
 });
